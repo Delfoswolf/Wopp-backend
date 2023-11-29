ba097150994b25306f96621a2b5b7599b4c90d0f
Camilo Chauta <camilog1706@gmail.com>
2023-11-29 18:54:35 -0500

    [+] Actualizacion con Atlas, creacion de enlace con la DB.

--- src/models/Product.js
+++ src/models/Product.js
@@ -1,4 +1,4 @@
-const { Schema, model } = require( 'mongoose' );
+ const { Schema, model } = require( 'mongoose' );
 
 const ProductSchema = new Schema({
     // Define las propiedades de la Entidad
